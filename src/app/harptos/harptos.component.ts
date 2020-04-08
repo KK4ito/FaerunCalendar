fe3028d9de3496cbaa564f578260f845a29d2fac
Kevin Lien <k4ito@outlook.com>
2020-04-08 22:55:10 +0200

    add snackbar and some red shades

--- src/app/harptos/harptos.component.ts
+++ src/app/harptos/harptos.component.ts
@@ -5,9 +5,11 @@ import { MatBottomSheet, MatBottomSheetConfig } from '@angular/material/bottom-s
 import { ActionDaySheetComponent } from '@app/action-day-sheet/action-day-sheet.component';
 import { FirebaseService } from '@app/firebase/firebase.service';
 import { MatTabChangeEvent } from '@angular/material/tabs';
-import { CalendarEntry } from '@app/calender-entry-dialog/calender-entry-dialog.component';
+import { CalendarEntry } from '@app/calender-entry-dialog/calendar-entry-dialog.component';
 import { CalendarEventEmitterService } from '@app/calendar-event-emitter/calendar-event-emitter.service';
 import { Subscription } from 'rxjs';
+import { LoadingEventEmitterService } from '@app/loading-event-emitter/loading-event-emitter.service';
+import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-harptos',
@@ -15,15 +17,20 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./harptos.component.scss'],
 })
 export class HarptosComponent implements OnInit, OnDestroy {
+  private readonly _redShades = ['#ff4c4c', '#ff2626', '#ff0000', '#b30000', '#660000'];
   private readonly _calendarData: HarptosMonth[];
   private _currentEvents: CalendarEntry[] = [];
   private _subscription: Subscription;
+  private _loadingSubscription: Subscription;
+  private _snackBarRef: MatSnackBarRef<SimpleSnackBar>;
 
   constructor(
+    private _snackBar: MatSnackBar,
     private _harptosService: HarptosService,
     private _bottomSheet: MatBottomSheet,
     private _firebaseService: FirebaseService,
-    private _calendarEventEmitterService: CalendarEventEmitterService
+    private _calendarEventEmitterService: CalendarEventEmitterService,
+    private _loadingEventEmitterService: LoadingEventEmitterService
   ) {
     this._calendarData = this._harptosService.calendarData;
   }
@@ -33,10 +40,18 @@ export class HarptosComponent implements OnInit, OnDestroy {
     this._subscription = this._calendarEventEmitterService.calenderEntriesSubject.subscribe(
       (currentEvents) => (this._currentEvents = currentEvents)
     );
+    this._loadingSubscription = this._loadingEventEmitterService.loadingInProgressSubject.subscribe((next) => {
+      if (next) {
+        this._snackBarRef = this._snackBar.open('Processing');
+      } else {
+        this._snackBarRef?.dismiss();
+      }
+    });
   }
 
   ngOnDestroy(): void {
     this._subscription.unsubscribe();
+    this._loadingSubscription.unsubscribe();
   }
 
   getHarptosData() {
@@ -62,7 +77,15 @@ export class HarptosComponent implements OnInit, OnDestroy {
     this._bottomSheet.open(ActionDaySheetComponent, config);
   }
 
-  checkIfEntryExistForSpecificDay(dayIndex: number): boolean {
-    return !(this._currentEvents.findIndex((value) => value.day === dayIndex) === -1);
+  checkIfEntryExistForSpecificDay(dayIndex: number): number {
+    return this._currentEvents.filter((value) => value.day === dayIndex).length;
+  }
+
+  getColorShade(dayIndex: number) {
+    const eventsForSpecificDay = this._currentEvents.filter((value) => value.day === dayIndex).length;
+    if (eventsForSpecificDay > this._redShades.length) {
+      return this._redShades[this._redShades.length - 1];
+    }
+    return this._redShades[eventsForSpecificDay - 1];
   }
 }
